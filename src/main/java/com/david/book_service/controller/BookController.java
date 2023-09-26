22b71519b550477f9c3241549c91576721816689
Dávid Halascsák <david.halascsak@gmail.com>
2023-09-26 19:32:10 +0200

    Update properties

--- src/main/java/com/david/book_service/controller/BookController.java
+++ src/main/java/com/david/book_service/controller/BookController.java
@@ -21,11 +21,6 @@ public class BookController {
         this.bookService = new BookService(bookRepository, mongoTemplate);
     }
 
-    @GetMapping("/hello")
-    public String getHello() {
-        return "Hello K8S";
-    }
-
     @GetMapping("/books")
     public List<Book> getBooks() {
         return bookService.getAllBooks();
