29872faf0baaae83224dad629a23715d13f3f79f
Dávid Halascsák <david.halascsak@gmail.com>
2024-04-20 22:21:02 +0200

    Add caching

---
+++ src/main/java/com/david/book_service/cache/CacheLoader.java
@@ -0,0 +1,38 @@
+package com.david.book_service.cache;
+
+import com.david.book_service.model.Book;
+import com.david.book_service.service.BookService;
+import jakarta.annotation.PostConstruct;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import java.util.List;
+
+@Slf4j
+@Component
+@EnableScheduling
+@AllArgsConstructor
+public class CacheLoader {
+
+    private BookService service;
+
+    @PostConstruct
+    public void loadCache() {
+        List<Book> topRatedBooks = service.getTopRatedBooks();
+        for(Book book: topRatedBooks) {
+            service.addToCache(book);
+        }
+        log.info("Top rated books are cached");
+    }
+
+    @Scheduled(cron ="0 0 0 * * *")
+    @CacheEvict(value = "books", allEntries = true)
+    public void updateCache()
+    {
+        log.info("Clear book cache");
+        loadCache();
+    }
+}
