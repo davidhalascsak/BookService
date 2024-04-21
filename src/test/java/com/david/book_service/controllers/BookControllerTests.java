6dfbb7c8d4d543fb7248439720ae37b7d7ce3504
Dávid Halascsák <david.halascsak@gmail.com>
2024-04-21 17:12:32 +0200

    Add endpoint tests to BookController

---
+++ src/test/java/com/david/book_service/controllers/BookControllerTests.java
@@ -0,0 +1,51 @@
+package com.david.book_service.controllers;
+
+import com.david.book_service.model.Book;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class BookControllerTests {
+
+    @Test
+    public void testGetTopRatedBooks() {
+        given()
+                .when()
+                .get("http://localhost:8100/books/topRated")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void testGetBooksByGenreExist() {
+        List<Book> books = given()
+                .when()
+                .pathParam("genre", "historical fiction")
+                .get("http://localhost:8100/books/genres/{genre}")
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .jsonPath().getList(".", Book.class);
+
+        assertThat(books.isEmpty(), is(false));
+    }
+
+    @Test
+    public void testGetBooksByGenreNotExist() {
+        List<Book> books = given()
+                .when()
+                .pathParam("genre", "historical cooking")
+                .get("http://localhost:8100/books/genres/{genre}")
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .jsonPath().getList(".", Book.class);
+
+        assertThat(books.isEmpty(), is(true));
+    }
+}
