9f86d2edec3298152afd2a31207f92f1224bf1c2
Dávid Halascsák <david.halascsak@gmail.com>
2024-04-30 15:08:35 +0200

    Refactor MongoConfig

--- src/main/java/com/david/book_service/MongoConfig.java
+++ src/main/java/com/david/book_service/MongoConfig.java
@@ -1,8 +1,11 @@
 package com.david.book_service;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 
@@ -11,19 +14,20 @@ import java.util.Base64;
 @Configuration
 public class MongoConfig extends AbstractMongoClientConfiguration {
 
-    @Value("${mongodb.username}")
+    @Value("${MONGODB_USERNAME}")
     private String username;
 
-    @Value("${mongodb.password}")
+    @Value("${MONGODB_PASSWORD}")
     private String password;
 
     @Override
-    public MongoClient mongoClient() {
+    protected void configureClientSettings(MongoClientSettings.Builder builder) {
         String decodedUsername = new String(Base64.getDecoder().decode(username));
         String decodedPassword = new String(Base64.getDecoder().decode(password));
 
         String uri = "mongodb+srv://" + decodedUsername + ":" + decodedPassword +"@webshop.hy2s4mk.mongodb.net";
-        return MongoClients.create(uri);
+
+        builder.applyConnectionString(new ConnectionString(uri));
     }
 
     @Override
